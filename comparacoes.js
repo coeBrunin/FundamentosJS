ee2a106c89ed4f8c243d6c1a68af241120b1fcd2
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 20:01:29 -0300

    create comparacoes

---
+++ comparacoes.js
@@ -0,0 +1,26 @@
+// == (comparação implicita) / compara apenas o valor
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+
+
+// === / compara o valor e o tipo de dado
+
+// const numeroE = 5;
+// const textoE = "5";
+
+// console.log(numeroE === textoE)
+
+
+// typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+
+// (comparação explicita) 
+
+Number()
+String()
