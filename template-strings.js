b404a48e6baecbea50aaaa6e7b9d5c8be9c4feb1
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 20:37:15 -0300

    create template-strings

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Bruno";
+const idade = 2023-2003;
+const cidadeDeNascimento = "Belo Horizonte"
+
+// const apresentação = "Meu nome é " + nome + ", tenho " + idade + " anos" + " e nasci em " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, tenho ${idade} anos e nasci em ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
