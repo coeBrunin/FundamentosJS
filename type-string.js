961a4da53546bbf921a066e6ae9f7766c2bace12
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 14:48:31 -0300

    init commit

---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, muyndo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meunome = "Bruno";
+
+//concatenação (+)
+
+console.log(citacao + meunome)
