0241dd076551f3138b929ee0eea369dee92c0daa
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 15:46:14 -0300

    create scripts.js

--- scripts.js
+++ scripts.js
@@ -28,15 +28,15 @@
 
 // const
 
-const forma = 'triangulo';
-const altura = 5;
-const comprimento = 7;
-let area;
+// const forma = 'triangulo';
+// const altura = 5;
+// const comprimento = 7;
+// let area;
 
-if (forma === 'quadrado'){
-    area = altura * comprimento;
-} else {
-    area = altura * comprimento / 2;
-}
+// if (forma === 'quadrado'){
+//     area = altura * comprimento;
+// } else {
+//     area = altura * comprimento / 2;
+// }
 
-console.log(area)
+// console.log(area)
