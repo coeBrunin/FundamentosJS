c7aa2e7e0f2fc1d4e4bdee808cc19f0e8351a5ae
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 16:15:03 -0300

    create conversoes

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + numeroString)
+
+// conversao explicita
+
+// number() => transforma em number
+// string() => transforma em string
+
+console.log(numero + Number(numeroString))
