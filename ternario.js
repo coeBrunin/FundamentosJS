2a6dd009a2eec39a138d34a6364a84d4bf34cdcc
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 20:12:28 -0300

    create ternario

---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeClient = 19;
+
+// if (idadeClient >= idadeMinima) {
+//     console.log("cerveja")
+// } else {
+//     console.log("suco")
+// }
+
+               // condição                //true     //false
+console.log(idadeClient >= idadeMinima ? "cerveja" : "suco")
