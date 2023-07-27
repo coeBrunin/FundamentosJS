5866b5c0a2cb35d49d2fe13c21b29de6a344b908
Bruno <goncalvesbruno445@gmail.com>
2023-07-26 21:23:47 -0300

    create parametros

---
+++ parametros.js
@@ -0,0 +1,26 @@
+// parâmetros de função
+
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+// console.log(soma(2, 2))
+// console.log(soma(256, 47))
+// console.log(soma(-1000, 2500))
+
+// parâmetros x argumentos
+
+// ordem dos parâmetros
+
+function nomeIdade(nome , idade) {
+    return `meu nome é ${nome} e minha idade é ${idade}`;
+}
+
+//console.log(nomeIdade())
+
+function multiplica(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+                            //9         //6
+console.log(multiplica(soma(4, 5), soma(3, 3)))
